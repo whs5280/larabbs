77a9bfa557fb3150a9b60c039371f167dbd13a7a
whs5280 <13434118350@163.com>
2020-09-21 20:15:15 +0800

    feat:裁剪头像

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,7 +31,7 @@ class UserController extends Controller
         $data = $request->all();
 
         if ($request->avatar) {
-            $result = $uploadHandler->save($request->avatar, 'avatars', $user->id);
+            $result = $uploadHandler->save($request->avatar, 'avatars', $user->id, 416);
             if ($result) {
                 $data['avatar'] = $result['path'];
             }
