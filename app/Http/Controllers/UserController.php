f527837fab5451e041c948f86fe42da3bc56d58f
whs5280 <13434118350@163.com>
2020-09-21 17:01:41 +0800

    feat:限制头像分辨率

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Handlers\ImageUploadHandler;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(UserRequest $request, ImageUploadHandler $uploadHandler, User $user)
+    {
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploadHandler->save($request->avatar, 'avatars', $user->id);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
+    }
+}
---
+++ database/migrations/2020_09_21_143044_add_avatar_and_introduction_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddAvatarAndIntroductionToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar')->nullable()->comment('头像');
+            $table->string('introduction')->nullable()->comment('个性签名');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
