6b57f4401ed1f8ab52c83c3d50870f1264ea21fb
whs5280 <13434118350@163.com>
2020-09-22 14:39:07 +0800

    feat:分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -9,3 +9,8 @@
     {
         return str_replace('.', '-', Route::currentRouteName());
     }
+
+    function category_nav_active($category_id)
+    {
+        return active_class((if_route('categories.show')) && if_route_param('category', $category_id));
+    }
