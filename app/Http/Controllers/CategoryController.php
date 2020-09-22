6f9f752508e73957768232c26aac98d0dbe24e56
whs5280 <13434118350@163.com>
2020-09-22 15:32:26 +0800

    feat:用户话题列表

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Topic;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function show(Category $category, Request $request, Topic $topic)
+    {
+        // 读取分类 ID 关联的话题，并按每 20 条分页
+        $topics = $topic->withOrder($request->order)
+            ->where('category_id', $category->id)
+            ->with('user', 'category')   // 预加载防止 N+1 问题
+            ->paginate(20);
+        return view('topics.index', compact($category, $topics));
+    }
+}
