b083c148a778bcaa07b2796969bf7d2ee02e94b9
whs5280 <13434118350@163.com>
2020-09-22 11:26:18 +0800

    feat:填充用户和话题数据

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        // 头像假数据
+        $avatars = [
+            'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png',
+        ];
+
+        // 生成数据集合
+        $users = factory(User::class)->times(10)->make()->each(function ($user, $index) use ($faker, $avatars){
+           //  从头像数组中随机取出一个并赋值
+            $user->avatar = $faker->randomElement($avatars);
+        });
+
+        // 让隐藏字段可见，并将数据集合转换为数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入到数据库中
+        DB::table('users')->insert($user_array);
+
+        // 单独处理第一个用户的数据
+        $user = User::find(2);
+        $user->name = 'whs5280';
+        $user->email = '13434118350@139.com';
+        $user->avatar = 'http://localhost/uploads/images/avatars/202009/21/2_1600678341_7OktgspkCN.jpeg';
+        $user->introduction = '起风了呀';
+        $user->save();
+    }
+}
