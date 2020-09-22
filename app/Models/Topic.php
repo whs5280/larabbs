c6ca05501bb5f8a331e24685487549ba9b443f2d
whs5280 <13434118350@163.com>
2020-09-22 12:05:11 +0800

    feat:话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,14 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id', 'id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
