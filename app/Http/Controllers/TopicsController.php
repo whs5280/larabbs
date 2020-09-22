edaa2f58864937e95c7c0403b226b0eefd8d64fc
whs5280 <13434118350@163.com>
2020-09-22 14:16:00 +0800

    feat:修复 N+1 问题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,7 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+		$topics = Topic::with(['category','user'])->paginate(30);
 		return view('topics.index', compact('topics'));
 	}
 
