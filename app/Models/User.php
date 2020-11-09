9c34e8720ceff2e1aa795dec12a89cce8a4c5db6
whs5280 <13434118350@163.com>
2020-11-09 19:23:42 +0800

    feat:管理员可以管理权限和角色

--- app/Models/User.php
+++ app/Models/User.php
@@ -88,7 +88,7 @@ class User extends Authenticatable
             $value = bcrypt($value);
         }
 
-        $value = bcrypt($value);
+        $this->attributes['password'] = $value;
     }
 
     public function setAvatarAttribute($path)
