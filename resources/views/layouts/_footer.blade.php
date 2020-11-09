ae895998487493a48288d7a365d28f133f2e2bd0
whs5280 <13434118350@163.com>
2020-11-09 19:33:36 +0800

    feat:站点设置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,6 +5,6 @@
             <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="float-right"><a href="">联系我们</a></p>
+        <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
