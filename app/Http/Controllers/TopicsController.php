2a55e09e2bace87569e562663ded90a39047720e
whs5280 <13434118350@163.com>
2020-11-10 10:47:41 +0800

    feat:侧边栏链接

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Handlers\ImageUploadHandler;
 use App\Models\Category;
+use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -18,14 +19,17 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic, User $user)
+	public function index(Request $request, Topic $topic, User $user, Link $link)
 	{
 		$topics = $topic->withOrder($request->order)
             ->with(['category','user']) // 预加载防止 N+1 问题
             ->paginate(20);
+
 		$active_users = $user->getActiveUsers();
 
-        return view('topics.index', compact('topics', 'active_users'));
+        $links = $link->getAllCached();
+
+        return view('topics.index', compact('topics', 'active_users', 'links'));
 	}
 
     public function show(Request $request, Topic $topic)
--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -6,7 +6,7 @@
     </div>
 </div>
 
-@if (count($active_users))
+@if (isset($active_users))
     <div class="card mt-4">
         <div class="card-body active-users pt-2">
             <div class="text-center mt-1 mb-0 text-muted">活跃用户</div>
@@ -18,6 +18,22 @@
                     </div>
                     <div class="media-body">
                         <small class="media-heading text-secondary">{{ $active_user->name }}</small>
+                    </div>
+                </a>
+            @endforeach
+        </div>
+    </div>
+@endif
+
+@if (count($links))
+    <div class="card mt-4">
+        <div class="card-body pt-2">
+            <div class="text-center mt-1 mb-0 text-muted">资源推荐</div>
+            <hr class="mt-2 mb-3">
+            @foreach ($links as $link)
+                <a class="media mt-1" href="{{ $link->link }}">
+                    <div class="media-body">
+                        <span class="media-heading text-muted">{{ $link->title }}</span>
                     </div>
                 </a>
             @endforeach
