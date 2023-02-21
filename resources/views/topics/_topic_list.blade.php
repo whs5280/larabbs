5c3456bca34804b3c45b827abf66d22cb2fdb45f
whs5280 <13434118350@163.com>
2023-02-21 10:07:05 +0800

    fix: 调整话题列表view的数据渲染

--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -4,7 +4,7 @@
             <li class="media">
                 <div class="media-left">
                     <a href="{{ route('users.show', [$topic->user_id]) }}">
-                        <img class="media-object img-thumbnail mr-3" style="width: 52px; height: 52px;" src="{{ $topic->user->avatar }}" title="{{ $topic->user->name }}">
+                        <img class="media-object img-thumbnail mr-3" style="width: 52px; height: 52px;" src="{{ $topic->user['avatar'] }}" title="{{ $topic->user['name'] }}">
                     </a>
                 </div>
 
@@ -21,19 +21,21 @@
 
                     <small class="media-body meta text-secondary">
 
-                        <a class="text-secondary" href="{{ route('categories.show', $topic->category_id) }}" title="{{ $topic->category->name }}">
+                        <a class="text-secondary" href="{{ route('categories.show', $topic->category_id) }}" title="{{ $topic->category['name'] }}">
                             <i class="far fa-folder"></i>
-                            {{ $topic->category->name }}
+                            {{ $topic->category['name'] }}
                         </a>
 
                         <span> • </span>
-                        <a class="text-secondary" href="{{ route('users.show', [$topic->user_id]) }}" title="{{ $topic->user->name }}">
+                        <a class="text-secondary" href="{{ route('users.show', [$topic->user_id]) }}" title="{{ $topic->user['name'] }}">
                             <i class="far fa-user"></i>
-                            {{ $topic->user->name }}
+                            {{ $topic->user['name']}}
                         </a>
                         <span> • </span>
                         <i class="far fa-clock"></i>
-                        <span class="timeago" title="最后活跃于：{{ $topic->updated_at }}">{{ $topic->updated_at->diffForHumans() }}</span>
+                        <span class="timeago" title="最后活跃于：{{ $topic->updated_at }}">
+                            {{ \Carbon\Carbon::parse($topic->updated_at)->diffForHumans() }}
+                        </span>
                     </small>
 
                 </div>
